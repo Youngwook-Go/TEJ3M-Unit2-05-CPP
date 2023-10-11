b981bf1528c9f53c2514da6b6f3c4cdf3888cb0b
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2023-10-11 14:59:55 +0000

    servo.cpp

---
+++ servo.cpp
@@ -0,0 +1,21 @@
+// Created by: Youngwook Go
+// Created on: OCT 2023
+// Moves a servo
+
+#include <Servo.h>
+
+Servo servoNumber1;
+
+void setup() {
+  // setup servo pins
+  servoNumber1.attach(2);
+  servonumber1.write(0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  servoNumber1.write(90);
+  delay(1000);
+  servoNumber1.write(0);
+  delay(1000);
+}
